42459e3c90874f9169cd4b8ba625b605c43127e2
fhh404 <406138227@qq.com>
2026-04-09 10:47:51 +0800

    第一次提交

---
+++ CADFP/CADFP/CADFPApp.swift
@@ -0,0 +1,17 @@
+//
+//  CADFPApp.swift
+//  CADFP
+//
+//  Created by huaheng feng on 4/9/26.
+//
+
+import SwiftUI
+
+@main
+struct CADFPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CADFP/CADFP/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CADFP
+//
+//  Created by huaheng feng on 4/9/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
